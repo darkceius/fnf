7d0a3caa1be8a3dd18177fafc3793c4bdc430daa
Darkceius <74603733+darkceius@users.noreply.github.com>
2023-02-23 00:17:57 +0200

    n

--- chartLoader.lua
+++ chartLoader.lua
@@ -3,59 +3,47 @@
 local chartLoader = {}
 
 local function chartLoader.playChart(chartData, offset, isBF)
-
 local ev1 = Instance.new("BindableEvent")
-
 local self = {}
-
 self.onNotePressed = ev1.Event
-
 self.offset = offset
-
 self.bf = isBF
 
 local alive = true
-
 local handler = task.spawn(function()
-
 for index, section in pairs(chartData.song.notes) do
 
 for index2, note in pairs(section.sectionNotes) do
 
 task.spawn(function()
+if ev[2] > 4 and self.bf == false then
+return
+end
+
+if ev[2] > 4 and self.bf then
+local b = table.clone(ev)
+b[2]=b[2]-4
+ev=b
+end
 
 wait((ev[1]/1000)-self.offset)
 
-if note[2] > 4 and self.bf == true then
-
-ev1:Fire({ev[1],ev[2]-4,ev[3])
-
-else
-
 ev1:Fire(note)
-
-end
 
 end)
 
 end
 
 end
-
 end)
 
 self.cancel = function()
-
 task.cancel(handler)
-
 alive = false
-
 return true
-
 end
 
 return self
-
 end
 
 return chartLoader
