6047ab0012f5874cfc876b951ad58720d4091e15
Darkceius <74603733+darkceius@users.noreply.github.com>
2023-02-23 00:47:10 +0200

    Update chartLoader.lua

--- chartLoader.lua
+++ chartLoader.lua
@@ -2,47 +2,48 @@
 
 local chartLoader = {}
 
-local function chartLoader.playChart(chartData, offset, isBF)
-local ev1 = Instance.new("BindableEvent")
-local self = {}
-self.onNotePressed = ev1.Event
-self.offset = offset
-self.bf = isBF
+function chartLoader.playChart(chartData, offset, isBF, maxNotes)
+   maxnotes = maxnotes or 3 -- 0 1 2 3
+   local ev1 = Instance.new("BindableEvent")
+   local self = {}
+   self.onNotePressed = ev1.Event
+   self.offset = offset
+   self.bf = isBF
 
-local alive = true
-local handler = task.spawn(function()
-for index, section in pairs(chartData.song.notes) do
+   local alive = true
+   local handler = task.spawn(function()
+   for index, section in pairs(chartData.song.notes) do
 
-for index2, note in pairs(section.sectionNotes) do
+      for index2, note in pairs(section.sectionNotes) do
 
-task.spawn(function()
-if ev[2] > 4 and self.bf == false then
-return
-end
+         task.spawn(function()
+         if ev[2] > maxNotes and self.bf == false then
+            return
+         end
 
-if ev[2] > 4 and self.bf then
-local b = table.clone(ev)
-b[2]=b[2]-4
-ev=b
-end
+         if ev[2] > maxNotes and self.bf then
+            local b = table.clone(ev)
+            b[2]=b[2]-(maxNotes+1)
+            ev=b
+         end
 
-if not alive then return end
-wait((ev[1]/1000)-self.offset)
-if not alive then return end
+         if not alive then return end
+         wait((ev[1]/1000)-self.offset)
+         if not alive then return end
 
-ev1:Fire(note)
+         ev1:Fire(note)
 
-end)
+         end)
 
-end
+      end
 
-end
-end)
+   end
+   end)
 
-self.cancel = function()
-task.cancel(handler)
-alive = false
-return true
+   self.cancel = function()
+   task.cancel(handler)
+   alive = false
+   return true
 end
 
 return self
